28c062b9ff803e1f7a9940fb6fbf5a6812495e4d
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-02 18:23:26 -0300

    commented server.js code

--- src/server.js
+++ src/server.js
@@ -1,23 +1,26 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const routes = require('./routes')
+/* IMPORTANDO MODULOS */ 
+const express = require('express')  // importamos o modulo express (servidor) na variavel express
+const nunjucks = require('nunjucks') 
+const routes = require('./routes') 
 const methodOverride = require('method-override')
 
-const server = express()
+const server = express() // criação do servidor na variável server
 
-server.use(express.urlencoded({ extended: true }))
-server.use(express.static('public'))
-server.use(methodOverride('_method'))
-server.use(routes)
+server.use(express.urlencoded({ extended: true })) // urlencoded é um parser das informações vindas no corpo da requisição
+server.use(express.static('public')) // serve arquivos estáticos da pasta public
+server.use(methodOverride('_method')) // método para que possamos utilizar PUT e DELETE nos forms
+server.use(routes) // rotas
 
-server.set("view engine", "njk")
+server.set("view engine", "njk") // setamos aqui a view engine (motor de visualização) para arquivos .njk
 
+//configuramos o nunjucks. primeiro argumento: caminho da pasta, segundo: configurações.
 nunjucks.configure("src/app/views", {
     express: server,
     autoescape: false,
     noCache: true
 })
 
+// escutando o servidor na porta 5000
 server.listen(5000, function() {
     console.log("server is running")
 })
