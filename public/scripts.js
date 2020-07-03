b6a961ed7bb9d4d66241d66c49efa377a5ee9aa8
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-03 14:20:45 -0300

    cash mask created

---
+++ public/scripts.js
@@ -0,0 +1,20 @@
+const input = document.querySelector('input[name="price"]')
+input = addEventListener("keydown", function(e){
+    
+    setTimeout(function(){
+
+        let { value } = e.target
+    
+        value = value.replace(/\D/g, "")
+
+        value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
+    
+        e.target.value = value
+
+        console.log(value)
+    },1)
+
+})
