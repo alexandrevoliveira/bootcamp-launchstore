b76c32f258bf9d1725ddfa9ea1b6f1630a61e470
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-03 12:24:31 -0300

    post logic inserted

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,10 @@ routes.get("/", function(req, res) {
 })
 
 routes.get("/products/create", ProductController.create)
+routes.post("/products", ProductController.post)
 
+
+// Alias
 routes.get("/ads/create", function(req, res) {
     return res.redirect("/products/create")
 })
