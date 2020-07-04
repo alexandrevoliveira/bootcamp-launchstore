7959b9522d058f62f8d4f1a9e102e78823233fe7
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-04 15:29:42 -0300

    ProductController & routes refactored

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -28,6 +28,17 @@ module.exports = {
         results = await Category.all()
         const categories = results.rows
 
+        return res.rendirect(`/products/${productId}`, { productId, categories })
+    },
+    async edit(req, res) {
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        results = await Category.all()
+        const categories = results.rows
+
         return res.render("products/create", { productId, categories })
+
     }
 }
