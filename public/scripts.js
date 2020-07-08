25b6757e30693d3fb6773272b026c080c285a1f6
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-08 17:49:05 -0300

    starting the construction of the files manager

--- public/scripts.js
+++ public/scripts.js
@@ -32,3 +32,43 @@ const Mask = {
         }).format(value/100)
     }
 }
+
+//função p/ gerenciar o upload de imagens no front
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event){
+        const { files: fileList } = event.target //files oriundos do input = event.target
+        const { uploadLimit } = PhotosUpload
+
+        //restrição para o primeiro upload de arquivos não ultrapassar o uploadLimit
+        if( fileList.length > uploadLimit ) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return
+        }
+
+        Array.from(fileList).forEach( file => {
+            
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.result)
+
+                const div = document.createElement('div')
+                div.classList.add('photo')
+
+                div.onclick = () => alert('remover')
+
+                div.appendChild(image)
+
+                document.querySelector('#photos-preview').appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+        
+
+    }
+}
