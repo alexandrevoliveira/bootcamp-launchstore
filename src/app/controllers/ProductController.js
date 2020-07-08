ab2b30986a671fb2344e64480f8f384659c9df24
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-07 21:36:05 -0300

    edit, put and delete controllers added / post controller refactored

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,3 +1,5 @@
+const { formatPrice } = require('../../lib/utils')
+
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 
@@ -22,23 +24,53 @@ module.exports = {
             if(req.body[key] == "") return res.send("Please, fill all fields.")
         }
 
+        req.body.price = req.body.price.replace(/\D/g, "")
+
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
-        results = await Category.all()
-        const categories = results.rows
 
-        return res.rendirect(`/products/${productId}`, { productId, categories })
+        return res.redirect(`/products/${productId}/edit`)
     },
     async edit(req, res) {
 
-        let results = await Product.create(req.body)
-        const productId = results.rows[0].id
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send("Product not found!")
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
 
         results = await Category.all()
         const categories = results.rows
 
-        return res.render("products/create", { productId, categories })
+        return res.render("products/edit", { product , categories })
 
+    },
+    async put(req, res) {
+
+        const values = Object.values(req.body)
+
+        for(let value of values) {
+            if(value == "") return res.send("Please, fill all fields.")
+        }
+
+        req.body.price = req.body.price.replace(/\D/g, "")
+
+        if(req.body.old_price != req.body.price) {
+            const oldProduct = await Product.find(req.body.id)
+
+            req.body.old_price = oldProduct.rows[0].price
+        }
+
+        await Product.update(req.body)
+
+        return res.redirect(`/products/${req.body.id}/edit`)
+    },
+    async delete(req, res) {
+        await Product.delete(req.body.id)
+
+        return res.redirect("/products/create")
     }
 }
