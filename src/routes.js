f0409d951b6fb076ca0c15b06f2f29e0c99cf00b
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-13 20:03:14 -0300

    multer middleware inserted into post & put routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 const ProductController = require('./app/controllers/ProductController')
+const multer = require('multer')
 
 routes.get("/", function(req, res) {
     return res.render('layout')
@@ -9,8 +10,8 @@ routes.get("/", function(req, res) {
 routes.get("/products/create", ProductController.create)
 routes.get("/products/:id/edit", ProductController.edit)
 
-routes.post("/products", ProductController.post)
-routes.put("/products", ProductController.put)
+routes.post("/products", multer.array("photos", 6), ProductController.post)
+routes.put("/products", multer.array("photos", 6), ProductController.put)
 routes.delete("/products", ProductController.delete)
 
 
