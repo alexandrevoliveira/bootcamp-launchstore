dc2dfe16ca374a7ad8ff602a5afd6dfbb0058160
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-07-14 00:08:04 -0300

    Product Controller modified. Promise Arrays used in post & edit

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,6 +2,7 @@ const { formatPrice } = require('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
+const File = require('../models/File')
 
 module.exports = {
     create(req, res) {
@@ -24,11 +25,17 @@ module.exports = {
             if(req.body[key] == "") return res.send("Please, fill all fields.")
         }
 
+        if(req.files.length == 0) {
+            return res.send('Please, send at least one image')
+        }
+               
         req.body.price = req.body.price.replace(/\D/g, "")
-
+        
         let results = await Product.create(req.body)
         const productId = results.rows[0].id
 
+        const filesPromise = req.files.map(file => {File.create({...file,product_id: productId})})
+        await Promise.all(filesPromise)
 
         return res.redirect(`/products/${productId}/edit`)
     },
@@ -42,10 +49,19 @@ module.exports = {
         product.old_price = formatPrice(product.old_price)
         product.price = formatPrice(product.price)
 
+        // get categories
         results = await Category.all()
         const categories = results.rows
 
-        return res.render("products/edit", { product , categories })
+        // get images
+        results = await Product.files(product.id)
+        let files = results.rows
+        files = files.map(file => ({
+            ...file,
+            src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+        }))
+
+        return res.render("products/edit", { product , categories, files })
 
     },
     async put(req, res) {
