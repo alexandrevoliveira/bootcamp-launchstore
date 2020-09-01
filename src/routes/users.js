229aca325e76443b500e4d0ebf778f3e81c48913
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-01 09:36:00 -0300

    route for user register post

--- src/routes/users.js
+++ src/routes/users.js
@@ -18,7 +18,7 @@ const UserController = require('../app/controllers/UserController')
 
 // user register
 routes.get('/register', UserController.registerForm)
-// routes.post('/register', UserController.post)
+routes.post('/register', UserController.post)
 
 // routes.get('/', UserController.show)
 // routes.put('/', UserController.update)
