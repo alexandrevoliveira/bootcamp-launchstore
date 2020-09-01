920b6df23c08547b317dbac666b4556cd6819f0b
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-01 09:38:17 -0300

    add: checks for existing email/cpf_cnpj fields, as well as password mismatch

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,38 @@
+const User = require('../models/User')
+
 module.exports = {
     registerForm(req, res) {
         return res.render("user/register")
+    },
+    async post(req, res) {
+        // check if has all fields
+        try{
+            const keys = Object.keys(req.body)
+    
+            for(let key of keys) {
+                if (req.body[key] == "") return res.send("Please, fill all fields!")
+            }
+    
+            // check if user exists [email, cpf_cnpj]
+            let { email, cpf_cnpj, password, passwordRepeat } = req.body
+
+            cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+
+            const user = await User.findOne({ 
+                where: { email },
+                or: { cpf_cnpj }
+            })
+    
+            if (user) res.send('User exists')
+            
+            // check if password match passwordRepeat\    
+            if(password != passwordRepeat)
+                res.send('Password mismatch')
+    
+            return res.send('Passed!')
+            
+        } catch(err) {
+            console.error(err)
+        }
     }
 }
