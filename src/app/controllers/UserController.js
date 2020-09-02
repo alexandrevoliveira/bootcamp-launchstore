82a494101ee2f1b485bdfcccc93f3dcdc511f142
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-02 20:33:47 -0300

    validators from post moved to src/app/validators/

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,32 +4,15 @@ module.exports = {
     registerForm(req, res) {
         return res.render("user/register")
     },
+    show(req, res) {
+        return res.send('ok, cadastrado')
+    },
     async post(req, res) {
-        // check if has all fields
         try{
-            const keys = Object.keys(req.body)
-    
-            for(let key of keys) {
-                if (req.body[key] == "") return res.send("Please, fill all fields!")
-            }
-    
-            // check if user exists [email, cpf_cnpj]
-            let { email, cpf_cnpj, password, passwordRepeat } = req.body
 
-            cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+            const userId = await User.create(req.body)
 
-            const user = await User.findOne({ 
-                where: { email },
-                or: { cpf_cnpj }
-            })
-    
-            if (user) res.send('User exists')
-            
-            // check if password match passwordRepeat\    
-            if(password != passwordRepeat)
-                res.send('Password mismatch')
-    
-            return res.send('Passed!')
+            return res.redirect('/users')
             
         } catch(err) {
             console.error(err)
