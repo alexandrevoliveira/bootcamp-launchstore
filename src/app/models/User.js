9ddecac003caf160973691a6de97f0d551af248b
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-02 20:42:35 -0300

    error message field created

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db')
+const { hash } = require('bcryptjs')
 
 module.exports = { 
     async findOne(filters){
@@ -13,11 +14,43 @@ module.exports = {
                 // 1 vez = SELECT * FROM users WHERE \ email = email
                 query += ` ${field} = '${filters[key][field]}'`
             })
-            console.log(query)
         })
 
         const results = await db.query(query)
 
         return results.rows[0]
+    },
+    async create(data) {
+        try {
+            const query = `
+                INSERT INTO users (
+                    name,
+                    email,
+                    password,
+                    cpf_cnpj,
+                    cep,
+                    address                
+                ) VALUES ($1, $2, $3, $4, $5, $6)
+                RETURNING id
+            `
+            
+            // hash of password
+            const passwordHash = await hash(data.password, 8)
+
+            const values = [
+                data.name,
+                data.email,
+                passwordHash,
+                data.cpf_cnpj.replace(/\D/g, ""),
+                data.cep.replace(/\D/g, ""),
+                data.address
+            ]
+
+            const results = await db.query(query, values)
+
+            return results.rows[0].id
+        } catch (err) {
+            console.error(err)
+        }
     }
 }
