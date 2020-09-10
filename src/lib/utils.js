7dd2f041ff8291f7bc5f55bb5fd4ae7a3c0ec2a4
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-09 21:49:44 -0300

    add: formatCpfCnpj and formatCep functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -25,5 +25,55 @@ module.exports = {
             style: 'currency',
             currency: 'BRL'
         }).format(price/100)
+    },
+    formatCpfCnpj(value) {
+        value = value.replace(/\D/g, "")
+
+        if(value.length > 14) {
+            value = value.slice(0, -1)
+        }
+
+        // check if is cnpj - 11.222.333/0001-11
+        if(value.length > 11) {
+            // 11.222333000111
+            value = value.replace(/(\d{2})(\d)/, "$1.$2")
+
+            // 11.222.333000111
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+            // 11.222.333/000111
+            value = value.replace(/(\d{3})(\d)/, "$1/$2")
+
+            // 11.222.333/0001-11
+            value = value.replace(/(\d{4})(\d)/, "$1-$2")
+
+            // ou
+            // value = value.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d)/, "$1.$2.$3/$4-$5")
+            // expressões regulares
+            // () ==> placeholder
+
+        } else {
+            // 111.22233344
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+            // 111.222.33344
+            value = value.replace(/(\d{3})(\d)/, "$1.$2")
+
+            // 111.222.333-44
+            value = value.replace(/(\d{3})(\d)/, "$1-$2")
+        }
+
+        return value
+    },
+    formatCep(value) {
+        value = value.replace(/\D/g, "")
+
+        if(value.length > 8) {
+            value = value.slice(0, -1)
+        }
+            
+        value = value.replace(/(\d{5})(\d)/, "$1-$2")
+
+        return value
     }
 }
