13e22a6ef64080b38ddc57c3c8578aa8b91e17b1
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-10 20:53:57 -0300

    code refactored and login/logout done

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -4,5 +4,16 @@ const Product = require('../models/Product')
 const File = require('../models/File')
 
 module.exports = {
+    loginForm(req, res) {
+        return res.render("session/login")
+    },
+    login(req, res) {
+        req.session.userId = req.user.id
 
+        return res.redirect("/users")
+    },
+    logout(req, res) {
+        req.session.destroy()
+        return res.redirect("/")
+    }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -17,7 +17,7 @@ routes.get("/ads/create", function(req, res) {
 })
 
 routes.get("/accounts", (req, res) => {
-    return res.redirect("/users/register")
+    return res.redirect("/users/login")
 })
 
 module.exports = routes
