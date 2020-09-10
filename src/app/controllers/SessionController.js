43539cbb093fd86a43839701b22d85e5ccb43e11
alexandre oliveira <alexandrevoliveira30@gmail.com>
2020-09-09 22:08:50 -0300

    add: user validators released

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,8 @@
+const { formatPrice } = require('../../lib/utils')
+
+const Product = require('../models/Product')
+const File = require('../models/File')
+
+module.exports = {
+
+}
--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,40 +1,96 @@
 const User = require('../models/User')
+const { compare } = require('bcryptjs')
 
-module.exports = {
-    async post(req, res, next) {
+function checkAllFields(body) {
+    // check if has all fields
+    const keys = Object.keys(body)
 
-        // check if has all fields
-        const keys = Object.keys(req.body)
-
-        for(let key of keys) {
-            if (req.body[key] == "") return res.render('user/register', {
-                user: req.body,
+    for(let key of keys) {
+        if (body[key] == "") {
+            return  {
+                user: body,
                 error: 'Please, fill all fields!'
-            })
+            }
         }
-
-        // check if user exists [email, cpf_cnpj]
-        let { email, cpf_cnpj, password, passwordRepeat } = req.body
-
-        cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
-
-        const user = await User.findOne({ 
-            where: { email },
-            or: { cpf_cnpj }
-        })
-
-        if (user) return res.render('user/register', {
-            user: req.body,
-            error: 'User already exists'
-        })
-        
-        // check if password match passwordRepeat\    
-        if(password != passwordRepeat)
-            return res.render('user/register', {
-                user: req.body,
-                error: 'Password mismatch'
-            })
-        
-        next()        
     }
+}
+async function show(req, res, next) {
+    
+    const { userId: id } = req.session
+    
+    const user = await User.findOne({ where: { id }})
+
+    if(!user) return res.render("user/register", {
+        error: "Usuário não encontrado!"
+    })
+
+    req.user = user
+
+    next()
+}
+async function post(req, res, next) {
+
+    const fillAllFields = checkAllFields(req.body)
+    if(fillAllFields) {
+        return res.render("user/register", fillAllFields)
+    }
+
+    // check if user exists [email, cpf_cnpj]
+    let { email, cpf_cnpj, password, passwordRepeat } = req.body
+
+    cpf_cnpj = cpf_cnpj.replace(/\D/g, "")
+
+    const user = await User.findOne({ 
+        where: { email },
+        or: { cpf_cnpj }
+    })
+
+    if (user) return res.render('user/register', {
+        user: req.body,
+        error: 'User already exists'
+    })
+    
+    // check if password match passwordRepeat\    
+    if(password != passwordRepeat)
+        return res.render('user/register', {
+            user: req.body,
+            error: 'Password mismatch'
+        })
+    
+    next()        
+}
+async function update(req, res, next) {
+    // all fields
+    const fillAllFields = checkAllFields(req.body)
+    if(fillAllFields) {
+        return res.render("user/index", fillAllFields)
+    }
+
+    const { id, password } = req.body
+
+    if(!password) return res.render("user/index", {
+        user: req.body,
+        error: "Coloque sua senha para atualizar seu cadastro."
+    })
+
+    const user = await User.findOne({ where: {id} })
+
+    const passed = await compare(password, user.password)
+
+    if(!passed) return res.render("user/index", {
+        user: req.body,
+        error: "Senha incorreta"
+    })
+
+    req.user = user
+
+    next()
+    // has password
+
+    // password match
+}
+module.exports = {
+    show,
+    post,
+    update
 }
